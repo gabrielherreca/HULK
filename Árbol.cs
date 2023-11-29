460992f4b705944546cb2469912108838f9296dc
gabrielherreca <132971921+gabrielherreca@users.noreply.github.com>
2023-11-28 19:14:01 -0800

    Update Árbol.cs

--- Árbol.cs
+++ Árbol.cs
@@ -573,26 +573,16 @@ public class Entorno
 
 public class Variable
 {
-    private string name; 
-    private object value ;
+    public string Name { get; private set;}
+    public object Value { get; private set;} 
 
     public Variable(string name, object value)
     {
-        this.name = name;
-        this.value = value;
+        this.Name = name;
+        this.Value = value;
     }
 
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }
-    }
-
-    public object Value
-    {
-        get { return value; }
-        set { this.value = value; }
-    }
+    
 }
 
 
